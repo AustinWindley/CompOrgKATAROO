f0c5d8e55e529415b21a4c350de76d9b31457908
awindley <arwindley@gmail.com>
2024-04-25 12:50:33 -0400

    Created functions for forward, backward, left, right, and turning off along with proper names for pins

--- wheels.py
+++ wheels.py
@@ -3,30 +3,66 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-#first wheel
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(14, GPIO.OUT)
 
-#Second wheel
-GPIO.setup(23, GPIO.OUT)
-GPIO.setup(24, GPIO.OUT)
+#pin labels
+pinr_forw = 14
+pinr_backw = 18
+pinl_forw = 23
+pinl_backw = 24
 
-#First wheel
-GPIO.output(18, GPIO.HIGH)
-GPIO.output(14, GPIO.LOW)
-#GPIO.output(23, GPIO.HIGH)
-#GPIO.output(24, GPIO.LOW)
+#first wheel setup
+GPIO.setup(pinr_forw, GPIO.OUT)
+GPIO.setup(pinr_backw, GPIO.OUT)
 
-print("ON")
-time.sleep(5)
-#first wheel
-GPIO.output(18, GPIO.LOW)
-GPIO.output(14, GPIO.HIGH)
-#second wheel
-#GPIO.output(23, GPIO.LOW)
-#GPIO.output(24, GPIO.HIGH)
-time.sleep(5)
-#make sure everything is off
-GPIO.output(24, GPIO.LOW)
-GPIO.output(14, GPIO.LOW)
-print("OFF")
+#Second wheel setup
+GPIO.setup(pinl_forw, GPIO.OUT)
+GPIO.setup(pinl_backw, GPIO.OUT)
+
+# moves the vehicle forward, input length in seconds
+def forward(length):
+    print("going forward")
+    GPIO.output(pinr_forw, GPIO.HIGH)
+    GPIO.output(pinl_forw, GPIO.HIGH)
+    time.sleep(length)
+    GPIO.output(pinr_forw, GPIO.LOW)
+    GPIO.output(pinl_forw, GPIO.LOW)
+    
+# moves the vehicle backward, input length in seconds
+def backward(length):
+    print("going backward")
+    GPIO.output(pinr_backw, GPIO.HIGH)
+    GPIO.output(pinl_backw, GPIO.HIGH)
+    time.sleep(length)
+    GPIO.output(pinr_backw, GPIO.LOW)
+    GPIO.output(pinl_backw, GPIO.LOW)
+
+# Turns the vehicle right, input turn length in seconds
+def turn_right(length):
+    print("turning right")
+    GPIO.output(pinr_forw, GPIO.HIGH)
+    GPIO.output(pinr_backw, GPIO.LOW)
+    time.sleep(length)
+    GPIO.output(pinr_forw, GPIO.LOW)
+
+# Turns the vehicle left, input turn length in seconds
+def turn_left(length):
+    print("turning left")
+    GPIO.output(pinl_forw, GPIO.HIGH)
+    GPIO.output(pinl_backw, GPIO.LOW)
+    time.sleep(length)
+    GPIO.output(pinl_forw, GPIO.LOW)
+    
+# Turns off all power to the wheels
+def turn_off():
+    GPIO.output(pinr_forw, GPIO.LOW)
+    GPIO.output(pinr_backw, GPIO.LOW)
+    GPIO.output(pinl_forw, GPIO.LOW)
+    GPIO.output(pinl_backw, GPIO.LOW)
+  
+forward(3)
+backward(3)  
+turn_right(3)
+turn_left(3)
+turn_off()
+
+    
